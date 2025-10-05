e30a6732be9aeb64429e69b91d22d856fd2a6f4c
edger477 <edgingforever@outlook.com>
2025-10-05 19:28:36 +0200

    v1.0.4

--- version.py
+++ version.py
@@ -2,9 +2,10 @@
 Version information for Restim Funscript Processor
 1.0.2 - fixed ramp calculation bug
 1.0.3 - added motion axis generation (E1-E4)
+1.0.4 - fixed output folder config; added option to not generate prostate files
 """
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __app_name__ = "Restim Funscript Processor"
 __description__ = "GUI application for processing funscript files for electrostimulation devices"
 __author__ = "Funscript Tools Project"
